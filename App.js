9e3367166df623b7a6c363858d2a6725c352ca5d
Hanna Lam <hannalam.momo@gmail.com>
2023-02-01 01:52:01 +0100

    initial entry

---
+++ App.js
@@ -0,0 +1,54 @@
+//test
+
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/feathers/Focus';
+import { Timer } from './src/feathers/Timer';
+import { FocusHistory } from './src/feathers/FocusHistory';
+
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
+
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState();
+  const [history, setHistory] = useState([]);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
+      ) : (
+        <Timer
+          fontsSubject={currentSubject}
+          onTimeEnd={(subject) => {
+            setHistory([...history, subject])
+          }}
+          clearSubject={() => setCurrentSubject(null)}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
+
+//jsx javascript markup
